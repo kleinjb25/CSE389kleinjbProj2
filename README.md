fcc615b3f61812a14fad58e7c012b4fe2266283f
kleinjb25 <kleinjb@miamioh.edu>
2025-04-16 08:46:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CSE389kleinjbProj2
+ Project 2 for CSE389 - Unreal Engine with C++
---
+++ Source/cse389kleinjbProj2/cse389kleinjbProj2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "cse389kleinjbProj2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, cse389kleinjbProj2, "cse389kleinjbProj2" );
